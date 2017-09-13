25013c9af7691be586b6d88f7719e687e0c97f12
bumiemac001 <itbobby@foxmail.com>
2017-09-13 13:39:12 +0800

    add webview

---
+++ app/src/main/java/com/bobbyzhang/bestblog/ColumnActivity.java
@@ -0,0 +1,110 @@
+package com.bobbyzhang.bestblog;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by bumiemac001 on 2017/9/13.
+ */
+
+public class ColumnActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_column);
+        Toast.makeText(getApplicationContext(),"欢迎回来！",Toast.LENGTH_SHORT).show();
+        openUrl();
+    }
+
+    private void openUrl(){
+        webView = (WebView) findViewById(R.id.wv);
+        webView.requestFocus();
+        webView.getSettings().setJavaScriptEnabled(true);//支持js
+        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        webView.loadUrl("http://blog.itbobby.top/");
+
+        //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // TODO Auto-generated method stub
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                // TODO Auto-generated method stub
+                if (newProgress == 100) {
+                    // 网页加载完成
+                } else {
+                    // 加载中
+
+                }
+
+            }
+        });
+    }
+
+
+    //改写物理按键——返回的逻辑
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if(keyCode==KeyEvent.KEYCODE_BACK)
+        {
+            if(webView.canGoBack())
+            {
+                webView.goBack();//返回上一页面
+                return true;
+            }
+            else
+            {
+                exitBy2Click(); // 调用双击退出函数
+            }
+        }
+//        return super.onKeyDown(keyCode, event);
+        return false;
+    }
+
+    /**
+     * 双击退出函数
+     */
+    private static Boolean isExit = false;
+
+
+    private void exitBy2Click() {
+        Timer tExit = null;
+        if (isExit == false) {
+            isExit = true; // 准备退出
+            Toast.makeText(this, "再按一次和我说拜拜~", Toast.LENGTH_SHORT).show();
+            tExit = new Timer();
+            tExit.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    isExit = false; // 取消退出
+                }
+            }, 2000); // 如果2秒钟内没有按下返回键，则启动定时器取消掉刚才执行的任务
+
+        } else {
+            finish();
+            System.exit(0);
+        }
+    }
+
+}
--- app/src/main/java/com/bobbyzhang/bestblog/HomeActivity.java
+++ app/src/main/java/com/bobbyzhang/bestblog/HomeActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
